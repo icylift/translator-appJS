75a9b9da94d6143be3c0e9908662455b60406945
Aaron Williams <polarizedcuriosity@gmail.com>
2024-02-22 17:14:19 -0800

    started to added th js scripts

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTag = document.querySelector("select");
+const icons = document.querySelector(".row i");
+const translateBtn = document.querySelector("button");
+
+selectTag.forEach((tag, id) => {
+  for()
+});
